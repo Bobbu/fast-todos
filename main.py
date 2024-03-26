62f94f0225b4ad82b38d806b0872eb07ec510a0f
Rob Daly <macbobbu@mac.com>
2024-03-26 10:24:53 -0400

    Essential persistence added with a SQLite DB for todos

--- main.py
+++ main.py
@@ -1,51 +1,68 @@
-from fastapi import FastAPI
-from models import Todo
+from datetime import datetime
+from fastapi import FastAPI, HTTPException
+from models import Todo, TodoIn
+from database import metadata, engine, database,todos
+
+metadata.create_all(engine)
 
 app = FastAPI()
 
+### Lifecycle Events ###
+
+#TODO on_event is deprecated, use lifespan event handlers instead.
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+#TODO on_event is deprecated, use lifespan event handlers instead.
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+### Routes ##############
 
 @app.get("/")
 async def root():
-    return {"message": "Hello To Båbø's World!!!"}
-
-todos = []
+    timePart = datetime.now().strftime("%H:%M:%S")
+    return {"message": f"Hello To Båbø's World at {timePart}!!!"}
 
 # Get all todos
-@app.get("/todos")
+@app.get("/todos", response_model=list[Todo])
 async def get_todos():
-    return {"todos": todos}
+    query = todos.select()
+    return await database.fetch_all(query)
 
 # Get single todo
-@app.get("/todos/{todo_id}")
+@app.get("/todos/{todo_id}", response_model=Todo)
 async def get_todo(todo_id: int):
-    for todo in todos:
-        if todo.id == todo_id:
-            return {"todo" : todo}
-    return {"message": "Todo not found"}
+    query = f"SELECT * FROM todos WHERE id = {todo_id}"
+    #TODO This is failing with a 500 server error if the passed in todo_id is not valid.
+    try:
+        return await database.fetch_one(query)
+    except:
+        raise HTTPException(status_code=404, detail="Item not found")
     
-
+#TODO .dict() is also deprecated
 # Create a todo
-@app.post("/todos")
-async def create_todo(todo: Todo):
-    todos.append(todo)
-    return {"message": "Todo has been added"}
+@app.post("/todos", response_model=Todo)
+async def create_todo(todo: TodoIn):
+    query = todos.insert().values(item=todo.item, completed=todo.completed)
+    last_record_id = await database.execute(query)
+    return {**todo.dict(), "id" : last_record_id}
 
 
 # Update a todo
-@app.put("/todos/{todo_id}")
-async def update_todo(todo_id: int, updated_todo: Todo):
-    for todo in todos:
-        if todo.id == todo_id:
-            todo.item = updated_todo.item
-            return {"todo" : todo}
-    return {"message": "Todo not found"}
+@app.put("/todos/{todo_id}", response_model=Todo)
+async def update_todo(todo_id: int, updated_todo: TodoIn):
+    query = f"UPDATE todos SET item = '{updated_todo.item}', completed = {updated_todo.completed} WHERE id = {todo_id}"
+    await database.execute(query)
+    new_query = f"SELECT * from todos where id = {todo_id}"
+    return await database.fetch_one(new_query)
 
 # Delete a todo
-@app.delete("/todos/{todo_id}")
+@app.delete("/todos/{todo_id}", response_model=list[Todo])
 async def delete_todo(todo_id: int):
-    for todo in todos:
-        if todo.id == todo_id:
-            todos.remove(todo)
-            return {"message" : "Todo has been DELETED"}
-    return {"message": "Todo not found"}
-
+    query = f"DELETE FROM todos WHERE id = {todo_id}"
+    await database.execute(query)
+    new_query = todos.select()
+    return await database.fetch_all(new_query)
--- models.py
+++ models.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
+# Use TodoIn when creating a todo, since the database will provide the ID upon insertion of the new ToDo.
+class TodoIn(BaseModel):
+    item: str
+    completed: bool
+    
 class Todo(BaseModel):
     id: int
     item: str
-    
+    completed: bool
